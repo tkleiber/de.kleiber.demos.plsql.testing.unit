fa14f043c2d11510ad2c0d1af063b42912970925
Torsten Kleiber <torsten.kleiber@web.de>
2023-11-13 20:05:13 +0100

    utPLSQL installation via startup scripts in the freepdb1 instance

--- src/init/01_utplsql_install.sh
+++ src/init/01_utplsql_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-curl -LOk $(curl --silent https://api.github.com/repos/utPLSQL/utPLSQL/releases/latest | awk '/browser_download_url/ { print $2 }' | grep ".zip\"" | sed 's/"//g')
+curl -LOk "$(curl --silent https://api.github.com/repos/utPLSQL/utPLSQL/releases/latest | awk '/browser_download_url/ { print $2 }' | grep ".zip\"" | sed 's/"//g')"
 unzip -q utPLSQL.zip
-pushd "utPLSQL/source"
+pushd "utPLSQL/source" || exit $?
 sqlplus sys/oracle@freepdb1 as sysdba @install_headless.sql utp3 oracle
