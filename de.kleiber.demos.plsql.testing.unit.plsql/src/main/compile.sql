afc9fc7dcc157e5fe4d61e5e9a14bb4f2d2c32a7
Torsten Kleiber <torsten.kleiber@web.de>
2024-06-11 21:56:06 +0200

    move user creation from sqlplus in oracle instant client to sqlcl docker container

--- de.kleiber.demos.plsql.testing.unit.plsql/src/main/compile.sql
+++
@@ -1,4 +0,0 @@
-PROMPT compile sources for schema &1
-@&2/main/award_bonus.prc
-@&2/main/betwnstr.fnc
-exit 0;
---
+++ sql_scripts/compile.sql
@@ -0,0 +1,4 @@
+PROMPT compile sources for schema &1
+@&2/award_bonus.prc
+@&2/betwnstr.fnc
+exit 0;
