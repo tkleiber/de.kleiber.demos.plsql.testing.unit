251881edd7104b02828dbba829901684c4b8e491
Torsten Kleiber <torsten.kleiber@web.de>
2024-10-14 23:06:11 +0200

    Install utPLSQL CLI

--- src/init/01_utplsql_install.sh
+++ src/init/01_utplsql_install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+echo "Install utPLSQL Core"
 curl -LOk "$(curl --silent https://api.github.com/repos/utPLSQL/utPLSQL/releases/latest | awk '/browser_download_url/ { print $2 }' | grep ".zip\"" | sed 's/"//g')"
 unzip -q utPLSQL.zip
 pushd "utPLSQL/source" || exit $?
 sqlplus sys/oracle@freepdb1 as sysdba @install_headless.sql utp3 oracle
+popd
+echo "Install utPLSQL CLI"
+pushd "/opt/oracle/oradata" || exit $?
+curl -LOk "$(curl --silent https://api.github.com/repos/utPLSQL/utPLSQL-cli/releases/latest | awk '/browser_download_url/ { print $2 }' | grep ".zip\"" | sed 's/"//g')"
+unzip -q utPLSQL-cli.zip
+popd
