9c8ac91dc36d57e7b6b1d83fc6ca90e13ff42ae8
Torsten Kleiber <torsten.kleiber@web.de>
2024-10-15 21:09:28 +0200

    put content of 03_check_for_java_home.sh into the pom.xml itself because of execution rights as user oracle

--- src/init/03_check_for_java_home.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while ! grep -q JAVA_HOME /opt/oracle/oradata/java.env; do
-  echo "JAVA_HOME not found yet in /opt/oracle/oradata/java.env, sleeping..."
-  sleep 5
-done
-echo "String found!"
